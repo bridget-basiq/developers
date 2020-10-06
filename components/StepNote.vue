a5f0eca368311d8157be04159085a0b70b22063f
Laurens van der Maas <laurensvdmaas@gmail.com>
2020-10-06 16:54:17 +0200

    feat(dev-portal): added step & step-note components

---
+++ components/StepNote.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="step-note flex pt-6 mt-6 border-t border-alt">
+    <Tag class="mr-6" :color="color" type="secondary">{{ title }}</Tag>
+    <slot />
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+import { Tag } from '@chargetrip/internal-vue-components'
+
+@Component({ components: { Tag } })
+export default class Steps extends Vue {
+  @Prop() title
+  @Prop() color
+}
+</script>
--- plugins/globals.js
+++ plugins/globals.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
+import { Button } from '@chargetrip/internal-vue-components'
 import CodeBlock from '~/components/CodeBlock.vue'
 import Table from '~/components/Table.vue'
+import Step from '~/components/Step.vue'
+import StepNote from '~/components/StepNote.vue'
 
 Vue.component('code-block', CodeBlock)
 Vue.component('property-table', Table)
+Vue.component('step', Step)
+Vue.component('step-note', StepNote)
+Vue.component('c-button', Button)
