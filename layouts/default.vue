cfa502559cf6b484e1a2b3cd4c2413f0bebb1252
Laurens van der Maas <laurensvdmaas@gmail.com>
2020-11-24 16:55:01 +0100

    fix(dev-portal): mt fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -89,16 +89,16 @@
         <div class="lg:px-8 px-6 lg-max:overflow-x-hidden">
           <Nuxt class="page mb-8" />
         </div>
-        <PrevNextNavigation v-if="sideNav" class="mt-auto" />
-        <div
-          class="p-6 lg:p-8 border-t flex items-center border-alt text-14 mt-auto last:mt-0"
-        >
-          <span class="icon icon-survey mr-3" />
-          <p>Was this article useful?</p>
-          <nav class="flex h-6 items-center font-semibold ml-auto">
-            <div class="underline">No</div>
-            <div class="underline ml-6 text-accent">Yes</div>
-          </nav>
+        <div class="mt-auto">
+          <PrevNextNavigation v-if="sideNav" />
+          <div class="p-6 lg:p-8 border-t flex items-center border-alt text-14">
+            <span class="icon icon-survey mr-3" />
+            <p>Was this article useful?</p>
+            <nav class="flex h-6 items-center font-semibold ml-auto">
+              <div class="underline">No</div>
+              <div class="underline ml-6 text-accent">Yes</div>
+            </nav>
+          </div>
         </div>
       </div>
       <aside
